06553d4e58a296c4314e08ffae71980f671f0d73
0xeudaimonia <selectthebest18@gmail.com>
2025-04-08 04:32:06 +0200

    add exception handling and fix coding style

--- src/types/charterAuction.ts
+++ src/types/charterAuction.ts
@@ -4,4 +4,33 @@ type InfoRowProps = {
   bold?: boolean;
 };
 
-export type { InfoRowProps };
+type NFTMetadataAttributes = {
+  trait_type: string;
+  value: string;
+};
+
+type NFTMetadata = {
+  name?: string;
+  image?: string;
+  attributes?: NFTMetadataAttributes[];
+};
+
+type ChartData = {
+  round: string;
+  price: number;
+  leftBid: number;
+  rightBid: number;
+};
+
+type BarData = {
+  round: string;
+  price: number;
+  fillValue: number;
+};
+
+type Position = {
+  rewarders: `0x${string}`[];
+  bidPrice: bigint;
+};
+
+export type { InfoRowProps, NFTMetadataAttributes, NFTMetadata, ChartData, BarData, Position };
