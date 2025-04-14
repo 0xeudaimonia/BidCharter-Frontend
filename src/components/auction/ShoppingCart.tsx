f3aad53ddaea1dbb19f0d88cdb13226026d8c259
0xeudaimonia <selectthebest18@gmail.com>
2025-04-14 04:46:39 +0200

    add approve functionality in mergePositions

--- src/components/auction/ShoppingCart.tsx
+++ src/components/auction/ShoppingCart.tsx
@@ -1,17 +1,49 @@
+import { useWriteContract } from "wagmi";
 import InfoRow from "./InfoRow";
-import { CharterAuctionTypes } from "@/src/types";
+import { CharterAuctionTypes, GeneralTypes } from "@/src/types";
+import { ERC20ABI } from "@/src/libs/abi/ERC20";
+import { Abi } from "viem";
 
 interface IProps {
   shoppingCart: CharterAuctionTypes.Position[];
   handleBidPosition: () => void;
   handleRemovePosition: (position: CharterAuctionTypes.Position) => void;
+  auctionAddress: `0x${string}`;
+  usdt: GeneralTypes.Usdt;
+  entryFee: bigint;
 }
 
 export default function ShoppingCart({
   shoppingCart,
+  auctionAddress,
+  usdt,
+  entryFee,
   handleBidPosition,
   handleRemovePosition,
 }: IProps) {
+  const { writeContract } = useWriteContract();
+
+  const handleApprove = () => {
+    writeContract(
+      {
+        address: usdt?.address as `0x${string}`,
+        abi: ERC20ABI as Abi,
+        functionName: "approve",
+        args: [
+          auctionAddress as `0x${string}`,
+          (entryFee * BigInt(shoppingCart.length)) as bigint,
+        ],
+      },
+      {
+        onSuccess: () => {
+          console.log("Approved");
+        },
+        onError: (error) => {
+          console.error("Error approving:", error);
+        },
+      }
+    );
+  };
   return (
     <div>
       <h3 className="text-sm mt-5 text-white font-bold">My Shopping Cart</h3>
@@ -44,10 +76,16 @@ export default function ShoppingCart({
 
       <InfoRow label="My New Position:" value="$12,521.00" />
 
-      <div className="text-center">
+      <div className="text-center flex items-center gap-3">
+        <button
+          onClick={handleApprove}
+          className="cursor-pointer mt-5 sm:w-auto w-full text-sm font-bold text-black bg-white rounded-[10px] px-5 py-3"
+        >
+          Approve
+        </button>
         <button
           onClick={handleBidPosition}
-          className="cursor-pointer mt-5 sm:w-auto w-full mx-auto text-sm font-bold text-black bg-white rounded-[10px] px-5 py-3"
+          className="cursor-pointer mt-5 sm:w-auto w-full text-sm font-bold text-black bg-white rounded-[10px] px-5 py-3"
         >
           Merge Positions
         </button>
